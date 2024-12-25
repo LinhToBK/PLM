8583be89b2ff655cee05e58e7cb3e470241d0103
Linh To Van <76928720+LinhToBK@users.noreply.github.com>
2024-12-25 17:05:52 +0700

    Add new label for purcahse tab

--- 03_GUI_User_Interface/3_5_Purchase/frmMakeNewPO.Designer.cs
+++ 03_GUI_User_Interface/3_5_Purchase/frmMakeNewPO.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnExit
@@ -41,20 +42,32 @@
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(184, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
             // frmMakeNewPO
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(559, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnExit);
             this.Name = "frmMakeNewPO";
             this.Text = "frmMakeNewPO";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
     }
 }
